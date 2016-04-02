116e981ff08e5eef05a29936836c1937ae0a64ec
Bryan Housel <bryan@mapbox.com>
2016-04-01 23:07:57 -0400

    WIP: Add test coverage, fix some errors

--- include/shelf-pack.hpp
+++ include/shelf-pack.hpp
@@ -9,15 +9,93 @@
 
 namespace mapbox {
 
+
+struct Bin {
+    inline explicit Bin() { };
+    inline explicit Bin(uint32_t w_, uint32_t h_) :
+        w(w_), h(h_) { };
+    inline explicit Bin(uint32_t w_, uint32_t h_, uint32_t x_, uint32_t y_) :
+        w(w_), h(h_), x(x_), y(y_) { };
+
+    uint32_t w;
+    uint32_t h;
+    uint32_t x;
+    uint32_t y;
+};
+
+
+class Shelf {
+public:
+    /**
+     * Create a new Shelf.
+     *
+     * @private
+     * @class  Shelf
+     * @param  {uint32_t}  y_   Top coordinate of the new shelf
+     * @param  {uint32_t}  w_   Width of the new shelf
+     * @param  {uint32_t}  h_   Height of the new shelf
+     * @example
+     * Shelf shelf(64, 512, 24);
+     */
+    inline explicit Shelf(uint32_t y_, uint32_t w_, uint32_t h_) :
+        x(0), y(y_), w(w_), h(h_), wfree(w_) { };
+
+    /**
+     * Allocate a single bin into the shelf.
+     *
+     * @private
+     * @param   {uint32_t}  w_   Width of the bin to allocate
+     * @param   {uint32_t}  h_   Height of the bin to allocate
+     * @returns {optional<Bin>}  Allocated bin object with `x`, `y`, `w`, `h` properties, or `nullopt` if allocation failed
+     * @example
+     * shelf.alloc(12, 16);
+     */
+    std::experimental::optional<Bin> alloc(uint32_t w_, uint32_t h_) {
+        if (w_ > wfree || h_ > h) {
+            return std::experimental::optional<Bin>();
+        }
+        uint32_t x_ = x;
+        x += w_;
+        wfree -= w_;
+        return std::experimental::optional<Bin>(w_, h_, x_, y);
+    };
+
+    /**
+     * Resize the shelf.
+     * The resize will fail if the requested width is smaller than the current shelf width.
+     *
+     * @private
+     * @param   {uint32_t}  w_  Requested new width of the shelf
+     * @returns {bool} true if resize succeeded, false if failed
+     * @example
+     * shelf.resize(512);
+     */
+    bool resize(uint32_t w_) {
+        if (w_ < w) {
+            return false;
+        }
+        wfree += (w_ - w);
+        w = w_;
+        return true;
+    };
+
+    uint32_t x;
+    uint32_t y;
+    uint32_t w;
+    uint32_t h;
+    uint32_t wfree;
+};
+
+
 class ShelfPack {
 public:
 
     struct ShelfPackOptions {
-        bool autoResize;
+        bool autoResize = false;
     };
 
     struct PackOptions {
-        bool inPlace;
+        bool inPlace = false;
     };
 
     /**
@@ -27,8 +105,8 @@ public:
      * http://clb.demon.fi/files/RectangleBinPack.pdf
      *
      * @class  ShelfPack
-     * @param  {uint32_t}  [w=64]  Initial width of the sprite
-     * @param  {uint32_t}  [h=64]  Initial width of the sprite
+     * @param  {uint32_t}  [w_=64]  Initial width of the sprite
+     * @param  {uint32_t}  [h_=64]  Initial width of the sprite
      * @param  {ShelfPackOptions}  [options]
      * @param  {bool} [options.autoResize=false]  If `true`, the sprite will automatically grow
      * @example
@@ -36,10 +114,10 @@ public:
      * options.autoResize = false;
      * ShelfPack sprite = new ShelfPack(64, 64, options);
      */
-    ShelfPack(uint32_t w = 0, uint32_t h = 0, ShelfPackOptions &options = NULL) {
-        this.w = w || 64;
-        this.h = h || 64;
-        this.autoResize = options && options.autoResize;
+    ShelfPack(uint32_t w_ = 0, uint32_t h_ = 0, const ShelfPackOptions &options = ShelfPackOptions) {
+        w = w_ ? w_ : 64;
+        h = h_ ? h_ : 64;
+        autoResize = options.autoResize;
     };
 
     ~ShelfPack() { };
@@ -48,32 +126,30 @@ public:
     /**
      * Batch pack multiple bins into the sprite.
      *
-     * @param   {vector<Bin>}   bins Array of requested bins - each object should have `width`, `height` (or `w`, `h`) properties
+     * @param   {vector<Bin>}   bins Array of requested bins - each object should have `w`, `h` properties
      * @param   {PackOptions}  [options]
      * @param   {bool} [options.inPlace=false] If `true`, the supplied bin objects will be updated inplace with `x` and `y` properties
      * @returns {Vector<Bin>}   Array of allocated bins - each bin is an object with `x`, `y`, `w`, `h` properties
      * @example
      * var bins = [
-     *     { id: 'a', width: 12, height: 12 },
-     *     { id: 'b', width: 12, height: 16 },
-     *     { id: 'c', width: 12, height: 24 }
+     *     { id: 'a', w: 12, h: 12 },
+     *     { id: 'b', w: 12, h: 16 },
+     *     { id: 'c', w: 12, h: 24 }
      * ];
      * ShelfPack::PackOptions options;
      * options.inPlace = false;
      * std::vector<Bin> results = sprite.pack(bins, options);
      */
-    std::vector<Bin> pack(std::vector<Bin> bins, PackOptions& options = NULL) {
+    std::vector<Bin> pack(std::vector<Bin> bins, const PackOptions &options = PackOptions) {
         std::vector<Bin> results;
 
         for (auto bin : bins) {
-            uint32_t w = bin.w || bin.width;
-            uint32_t h = bin.h || bin.height;
-            if (w && h) {
-                std::experimental::optional<Bin> allocation = this.packOne(w, h);
+            if (bin.w && bin.h) {
+                std::experimental::optional<Bin> allocation = packOne(bin.w, bin.h);
                 if (!allocation) {
                     continue;
                 }
-                if (options && options.inPlace) {
+                if (options.inPlace) {
                     bin.x = allocation.x;
                     bin.y = allocation.y;
                 }
@@ -88,32 +164,32 @@ public:
     /**
      * Pack a single bin into the sprite.
      *
-     * @param   {uint32_t}  w   Width of the bin to allocate
-     * @param   {uint32_t}  h   Height of the bin to allocate
+     * @param   {uint32_t}  w_   Width of the bin to allocate
+     * @param   {uint32_t}  h_   Height of the bin to allocate
      * @returns {optional<Bin>}  Allocated bin object with `x`, `y`, `w`, `h` properties, or `nullopt` if allocation failed
      * @example
      * optional<Bin> results = sprite.packOne(12, 16);
      */
-    std::experimental::optional<Bin> packOne(uint32_t w, uint32_t h) {
-        uint32_t y = 0;
+    std::experimental::optional<Bin> packOne(uint32_t w_, uint32_t h_) {
+        uint32_t y_ = 0;
         struct { Shelf& shelf = NULL, uint32_t waste = UINT32_MAX } best;
 
         // find the best shelf
-        for (auto& shelf : this.shelves) {
-            y += shelf.h;
+        for (auto& shelf : shelves) {
+            y_ += shelf.h;
 
             // exactly the right height with width to spare, pack it..
-            if (h === shelf.h && w <= shelf.free) {
-                this.count(h);
-                return shelf.alloc(w, h);
+            if (h_ == shelf.h && w_ <= shelf.wfree) {
+                count(h_);
+                return shelf.alloc(w_, h_);
             }
             // not enough height or width, skip it..
-            if (h > shelf.h || w > shelf.free) {
+            if (h_ > shelf.h || w_ > shelf.wfree) {
                 continue;
             }
             // maybe enough height or width, minimize waste..
-            if (h < shelf.h && w <= shelf.free) {
-                uint32_t waste = shelf.h - h;
+            if (h_ < shelf.h && w_ <= shelf.wfree) {
+                uint32_t waste = shelf.h - h_;
                 if (waste < best.waste) {
                     best.waste = waste;
                     best.shelf = shelf;
@@ -122,15 +198,15 @@ public:
         }
 
         if (best.shelf) {
-            this.count(h);
-            return best.shelf.alloc(w, h);
+            count(h_);
+            return best.shelf.alloc(w_, h_);
         }
 
         // add shelf..
-        if (h <= (this.h - y) && w <= this.w) {
-            best.shelf = this.shelves.emplace_back(y, this.w, h);
-            this.count(h);
-            return best.shelf.alloc(w, h);
+        if (h_ <= (h - y_) && w_ <= w) {
+            best.shelf = shelves.emplace_back(y_, w, h_);
+            count(h_);
+            return best.shelf.alloc(w_, h_);
         }
 
         // no more space..
@@ -138,21 +214,21 @@ public:
         //  * double whichever sprite dimension is smaller (`w1` or `h1`)
         //  * if sprite dimensions are equal, grow width before height
         //  * accomodate very large bin requests (big `w` or `h`)
-        if (this.autoResize) {
+        if (autoResize) {
             var h1, h2, w1, w2;
 
-            h1 = h2 = this.h;
-            w1 = w2 = this.w;
+            h1 = h2 = h;
+            w1 = w2 = w;
 
-            if (w1 <= h1 || w > w1) {   // grow width..
-                w2 = std::max(w, w1) * 2;
+            if (w1 <= h1 || w_ > w1) {   // grow width..
+                w2 = std::max(w_, w1) * 2;
             }
-            if (h1 < w1 || h > h1) {    // grow height..
-                h2 = std::max(h, h1) * 2;
+            if (h1 < w1 || h_ > h1) {    // grow height..
+                h2 = std::max(h_, h1) * 2;
             }
 
-            this.resize(w2, h2);
-            return this.packOne(w, h);  // retry
+            resize(w2, h2);
+            return packOne(w_, h_);  // retry
         }
 
         return std::experimental::optional<Bin>();
@@ -165,28 +241,28 @@ public:
      * sprite.clear();
      */
     void clear() {
-        this.shelves = [];
-        this.stats = {};
+        shelves.clear();
+        stats.clear();
     };
 
     /**
      * Resize the sprite.
      * The resize will fail if the requested dimensions are smaller than the current sprite dimensions.
      *
-     * @param   {uint32_t}  w  Requested new sprite width
-     * @param   {uint32_t}  h  Requested new sprite height
+     * @param   {uint32_t}  w_  Requested new sprite width
+     * @param   {uint32_t}  h_  Requested new sprite height
      * @returns {bool} `true` if resize succeeded, `false` if failed
      * @example
      * sprite.resize(256, 256);
      */
-    bool resize(uint32_t w, uint32_t h) {
-        if (w < this.w || h < this.h) {
+    bool resize(uint32_t w_, uint32_t h_) {
+        if (w_ < w || h_ < h) {
             return false;
         }
 
-        this.w = w;
-        this.h = h;
-        for (auto& shelf : this.shelves) {
+        w = w_;
+        h = h_;
+        for (auto& shelf : shelves) {
             shelf.resize(w);
         }
         return true;
@@ -198,91 +274,13 @@ public:
 
 private:
     void count(uint32_t h) {
-        this.stats[h]++;
+       stats[h]++;
     };
 
     bool autoResize;
     std::vector<Shelf> shelves;
     std::map<uint32_t, uint32_t> stats;
 
-};
-
-
-
-class Shelf {
-public:
-    /**
-     * Create a new Shelf.
-     *
-     * @private
-     * @class  Shelf
-     * @param  {uint32_t}  y   Top coordinate of the new shelf
-     * @param  {uint32_t}  w   Width of the new shelf
-     * @param  {uint32_t}  h   Height of the new shelf
-     * @example
-     * Shelf shelf(64, 512, 24);
-     */
-    inline explicit Shelf(uint32_t y, uint32_t w, uint32_t h) :
-        x(0), y(y), w(w), free(w), h(h) { };
-
-    /**
-     * Allocate a single bin into the shelf.
-     *
-     * @private
-     * @param   {uint32_t}  w   Width of the bin to allocate
-     * @param   {uint32_t}  h   Height of the bin to allocate
-     * @returns {optional<Bin>}  Allocated bin object with `x`, `y`, `w`, `h` properties, or `nullopt` if allocation failed
-     * @example
-     * shelf.alloc(12, 16);
-     */
-    std::experimental::optional<Bin> alloc(uint32_t w, uint32_t h) {
-        if (w > this.free || h > this.h) {
-            return std::experimental::optional<Bin>();
-        }
-        var x = this.x;
-        this.x += w;
-        this.free -= w;
-        return std::experimental::optional<Bin>(x, this.y, w, h);
-    };
-
-    /**
-     * Resize the shelf.
-     * The resize will fail if the requested width is smaller than the current shelf width.
-     *
-     * @private
-     * @param   {uint32_t}  w  Requested new width of the shelf
-     * @returns {bool} true if resize succeeded, false if failed
-     * @example
-     * shelf.resize(512);
-     */
-    bool resize(uint32_t w) {
-        if (w < this.w) {
-            return false;
-        }
-        this.free += (w - this.w);
-        this.w = w;
-        return true;
-    };
-
-    uint32_t x;
-    uint32_t y;
-    uint32_t w;
-    uint32_t h;
-    uint32_t free;
-};
-
-
-struct Bin {
-    inline explicit Bin() { };
-    inline explicit Bin(uint32_t x, uint32_t y, uint32_t w, uint32_t h) :
-         x(x), y(y), w(w), h(h), width(w), height(h) { };
-
-    uint32_t x;
-    uint32_t y;
-    uint32_t w;
-    uint32_t h;
-    uint32_t width;
-    uint32_t height;
 };
 
 
---
+++ test/test.cpp
@@ -0,0 +1,296 @@
+#include <shelf-pack.hpp>
+
+#include <cassert>
+#include <iostream>
+
+
+void testPack1() {
+    std::cout << "batch pack allocates same height bins on existing shelf";
+
+    ShelfPack sprite(64, 64);
+    std::vector<Bin> bins, results;
+
+    bins.emplace_back(10, 10);
+    bins.emplace_back(10, 10);
+    bins.emplace_back(10, 10);
+    results = sprite.pack(bins);
+
+    assert(results[0].x == 0  && results[0].y == 0 && results[0].w == 10 && results[0].h == 10);
+    assert(results[1].x == 10 && results[1].y == 0 && results[1].w == 10 && results[1].h == 10);
+    assert(results[2].x == 20 && results[2].y == 0 && results[2].w == 10 && results[2].h == 10);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testPack2() {
+    std::cout << "batch pack allocates larger bins on new shelf";
+
+    ShelfPack sprite(64, 64);
+    std::vector<Bin> bins, results;
+
+    bins.emplace_back(10, 10);
+    bins.emplace_back(10, 15);
+    bins.emplace_back(10, 20);
+    results = sprite.pack(bins);
+
+    assert(results[0].x == 0 && results[0].y == 0  && results[0].w == 10 && results[0].h == 10);
+    assert(results[1].x == 0 && results[1].y == 10 && results[1].w == 10 && results[1].h == 15);
+    assert(results[2].x == 0 && results[2].y == 25 && results[2].w == 10 && results[2].h == 20);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testPack3() {
+    std::cout << "batch pack allocates shorter bins on existing shelf, minimizing waste";
+
+    ShelfPack sprite(64, 64);
+    std::vector<Bin> bins, results;
+
+    bins.emplace_back(10, 10);
+    bins.emplace_back(10, 15);
+    bins.emplace_back(10, 20);
+    bins.emplace_back(10,  9);
+    results = sprite.pack(bins);
+
+    assert(results[0].x == 0  && results[0].y == 0  && results[0].w == 10 && results[0].h == 10);
+    assert(results[1].x == 0  && results[1].y == 10 && results[1].w == 10 && results[1].h == 15);
+    assert(results[2].x == 0  && results[2].y == 25 && results[2].w == 10 && results[2].h == 20);
+    assert(results[3].x == 10 && results[3].y == 0  && results[3].w == 10 && results[3].h == 9 );
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testPack4() {
+    std::cout << "batch pack sets `x`, `y` properties to bins with `inPlace` option";
+
+    ShelfPack sprite(64, 64);
+    std::vector<Bin> bins;
+
+    ShelfPack::PackOptions options;
+    options.inPlace = true;
+
+    bins.emplace_back(10, 10);
+    bins.emplace_back(10, 10);
+    bins.emplace_back(10, 10);
+    sprite.pack(bins, options);
+
+    assert(bins[0].x == 0  && bins[0].y == 0 && bins[0].w == 10 && bins[0].h == 10);
+    assert(bins[1].x == 10 && bins[1].y == 0 && bins[1].w == 10 && bins[1].h == 10);
+    assert(bins[2].x == 20 && bins[2].y == 0 && bins[2].w == 10 && bins[2].h == 10);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testPack5() {
+    std::cout << "batch pack skips bins if not enough room";
+
+    ShelfPack sprite(20, 20);
+    std::vector<Bin> bins, results;
+
+    ShelfPack::PackOptions options;
+    options.inPlace = true;
+
+    bins.emplace_back(10, 10);
+    bins.emplace_back(10, 10);
+    bins.emplace_back(10, 30);   // should skip
+    bins.emplace_back(10, 10);
+    results = sprite.pack(bins, options);
+
+    assert(results[0].x == 0  && results[0].y == 0  && results[0].w == 10 && results[0].h == 10);
+    assert(results[1].x == 10 && results[1].y == 0  && results[1].w == 10 && results[1].h == 10);
+    assert(results[2].x == 0  && results[2].y == 10 && results[2].w == 10 && results[2].h == 10);
+
+    assert(bins[0].x == 0  && bins[0].y == 0  && bins[0].w == 10 && bins[0].h == 10);
+    assert(bins[1].x == 10 && bins[1].y == 0  && bins[1].w == 10 && bins[1].h == 10);
+    assert(bins[2].x == NULL  && bins[2].y == NULL && bins[2].w == 10 && bins[2].h == 30);
+    assert(bins[3].x == 0  && bins[3].y == 10 && bins[3].w == 10 && bins[3].h == 10);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testPackOne1() {
+    std::cout << "packOne allocates same height bins on existing shelf";
+
+    ShelfPack sprite(64, 64);
+    Bin bin;
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0  && bin.y == 0 && bin.w == 10 && bin.h == 10);
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 10 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 20 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testPackOne2() {
+    std::cout << "packOne allocates larger bins on new shelf";
+
+    ShelfPack sprite(64, 64);
+    Bin bin;
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 0  && bin.w == 10 && bin.h == 10);
+    bin = sprite.packOne(10, 15);
+    assert(bin.x == 0 && bin.y == 10 && bin.w == 10 && bin.h == 15);
+    bin = sprite.packOne(10, 20);
+    assert(bin.x == 0 && bin.y == 25 && bin.w == 10 && bin.h == 20);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testPackOne3() {
+    std::cout << "packOne allocates shorter bins on existing shelf, minimizing waste";
+
+    ShelfPack sprite(64, 64);
+    Bin bin;
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 0  && bin.w == 10 && bin.h == 10);
+    bin = sprite.packOne(10, 15);
+    assert(bin.x == 0 && bin.y == 10 && bin.w == 10 && bin.h == 15);
+    bin = sprite.packOne(10, 20);
+    assert(bin.x == 0 && bin.y == 25 && bin.w == 10 && bin.h == 20);
+    bin = sprite.packOne(10, 9);
+    assert(bin.x == 10 && bin.y == 0 && bin.w == 10 && bin.h == 9);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testNotEnoughRoom() {
+    std::cout << "not enough room";
+
+    ShelfPack sprite(10, 10);
+    Bin bin;
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 0  && bin.w == 10 && bin.h == 10);
+    bin = sprite.packOne(10, 10);
+    assert(bin == nullopt);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testAutoResize1() {
+    std::cout << "autoResize grows sprite dimensions by width then height";
+
+    ShelfPack::PackOptions options;
+    options.autoResize = true;
+
+    ShelfPack sprite(10, 10, options);
+    Bin bin;
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+    assert(sprite.w == 10 && sprite.h == 10);
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 10 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+    assert(sprite.w == 20 && sprite.h == 10);
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 10 && bin.w == 10 && bin.h == 10);
+    assert(sprite.w == 20 && sprite.h == 20);
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 10 && bin.y == 10 && bin.w == 10 && bin.h == 10);
+    assert(sprite.w == 20 && sprite.h == 20);
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 20 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+    assert(sprite.w == 40 && sprite.h == 10);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testAutoResize2() {
+    std::cout << "autoResize accommodates big bin requests";
+
+    ShelfPack::PackOptions options;
+    options.autoResize = true;
+
+    ShelfPack sprite(10, 10, options);
+    Bin bin;
+
+    bin = sprite.packOne(20, 10);
+    assert(bin.x == 0 && bin.y == 0 && bin.w == 20 && bin.h == 10);
+    assert(sprite.w == 40 && sprite.h == 10);
+
+    bin = sprite.packOne(10, 40);
+    assert(bin.x == 0 && bin.y == 10 && bin.w == 10 && bin.h == 40);
+    assert(sprite.w == 40 && sprite.h == 80);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testClear() {
+    std::cout << "clear succeeds";
+
+    ShelfPack sprite(10, 10);
+    Bin bin;
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+    bin = sprite.packOne(10, 10);
+    assert(bin == nullopt);
+
+    sprite.clear();
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testResizeLarger() {
+    std::cout << "resize larger succeeds";
+
+    ShelfPack sprite(10, 10);
+    Bin bin;
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+    assert(sprite.resize(20, 10) == true);   // grow width
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 10 && bin.y == 0 && bin.w == 10 && bin.h == 10);
+    assert(sprite.resize(20, 20) == true);   // grow height
+
+    bin = sprite.packOne(10, 10);
+    assert(bin.x == 0 && bin.y == 10 && bin.w == 10 && bin.h == 10);
+
+    std::cout << " - OK" << std::endl;
+}
+
+void testResizeSmaller() {
+    std::cout << "resize smaller fails";
+
+    ShelfPack sprite(10, 10);
+    assert(sprite.resize(9, 10) == false);   // shrink width
+    assert(sprite.resize(10, 9) == false);   // shrink height
+
+    std::cout << " - OK" << std::endl;
+}
+
+int main() {
+    testPack1();
+    testPack2();
+    testPack3();
+    testPack4();
+    testPack5();
+
+    testPackOne1();
+    testPackOne2();
+    testPackOne3();
+
+    testNotEnoughRoom();
+
+    testAutoResize1();
+    testAutoResize2();
+
+    testClear();
+    testResizeLarger();
+    testResizeSmaller();
+
+    return 0;
+}
