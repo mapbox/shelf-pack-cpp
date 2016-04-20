d697af4d9e7920842a9ae2ffc687ace9c67fdd1e
Bryan Housel <bryan@mapbox.com>
2016-04-20 12:23:32 -0400

    Try CircleCI parallel builds of g++ 4.9 and clang 3.4

---
+++ ci.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+case $CIRCLE_NODE_INDEX in
+    0)
+    export CXX="g++-4.9" CC="gcc-4.9"
+    ;;
+    *)
+    export CXX="clang++" CC="clang++"
+    ;;
+esac
+
+make clean
+make
